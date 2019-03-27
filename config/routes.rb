b3e2e3ad86970d167f14cf5cc2ffc756cbb9ef7c
Maurice Junior <maurice.junior.tchangue.imele@mni.thm.de>
2019-03-27 02:19:37 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get 'static_pages/help', to: 'static_pages#help', as: 'help'
-  get 'static_pages/about', to: 'static_pages#about', as: 'about'
-  get 'static_pages/login', to: 'static_pages#login', as: 'login'
-  get 'static_pages/signup', to: 'static_pages#signup', as: 'signup'
-  get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
+  get '/help', to: 'static_pages#help', as: 'help'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/login', to: 'static_pages#login', as: 'login'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
+
 end
