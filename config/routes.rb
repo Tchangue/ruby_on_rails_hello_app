d6faa2b5f6bd1acbab8d7915c1793c9506ddda82
Maurice Junior <maurice.junior.tchangue.imele@mni.thm.de>
2019-04-02 05:29:08 +0200

    files updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to: 'static_pages#help', as: 'help'
   get '/about', to: 'static_pages#about', as: 'about'
-  get '/login', to: 'static_pages#login', as: 'login'
+  get '/login', to: 'users#login', as: 'login'
   get '/contact', to: 'static_pages#contact', as: 'contact'
-  get '/signup', to: 'users#new'
+  get '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
 
