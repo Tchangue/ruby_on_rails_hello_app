216b84b8f82a4801523e146e618aaac7f9a03bd1
Maurice Junior <maurice.junior.tchangue.imele@mni.thm.de>
2019-04-12 01:52:27 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,20 @@ class UsersController < ApplicationController
     render '_profile'
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
 
